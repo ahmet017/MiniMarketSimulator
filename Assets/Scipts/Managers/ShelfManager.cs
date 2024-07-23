0c3853936165724a223e87e4c0ddf22dbd5288f4
ahmet017 <ahmetanilalper@gmail.com>
2024-07-23 15:12:27 +0300

    Better shelving system

--- Assets/Scipts/Managers/ShelfManager.cs
+++ Assets/Scipts/Managers/ShelfManager.cs
@@ -2,10 +2,11 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShelfManager : MonoBehaviour
 {
-
+    public Foods ActiveFood;
     public Transform shelf; // the shelf game object
     public float shelfHeight = 1.0f; // the height of the shelf
     private List<GameObject> itemsOnShelf = new List<GameObject>(); // list of items currently on the shelf
@@ -16,17 +17,27 @@ public class ShelfManager : MonoBehaviour
 
     public void PlaceItemOnShelf(GameObject item)
     {
+
         itemsOnShelf.Clear();
         foreach (Transform child in shelf)
         {
             if (child.gameObject.activeInHierarchy)
             {
                 itemsOnShelf.Add(child.gameObject);
+                ActiveFood = child.GetComponent<ItemId>().Item;
             }
 
+
+
+        }
+
+        // If there are no child objects, set ActiveFood to null
+        if (itemsOnShelf.Count == 0)
+        {
+            ActiveFood = null;
         }
         Foods SelectedItem = item.GetComponent<ItemId>().Item;
-        if (itemsOnShelf.Count < SelectedItem.MaxItemCountOnShelf)
+        if ((SelectedItem == ActiveFood || ActiveFood == null) && itemsOnShelf.Count < SelectedItem.MaxItemCountOnShelf)
         {
             
             int currentRow = itemsOnShelf.Count / (SelectedItem.MaxItemCountOnShelf/ 2);
@@ -37,7 +48,7 @@ public class ShelfManager : MonoBehaviour
             item.layer = 0;
             item.transform.parent = shelf;
             item.transform.localRotation = Quaternion.identity;
-            item.transform.localPosition = new Vector3(xPosition, -0.55f, (currentRow % 2 == 0) ? -0.28f : 0.28f);
+            item.transform.localPosition = new Vector3(xPosition, -0.58f, (currentRow % 2 == 0) ? -0.28f : 0.28f);
             item.transform.localScale = SelectedItem.scale;
 
             FirstPersonController.Instance.DropItem(0);
